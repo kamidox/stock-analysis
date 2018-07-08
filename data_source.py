c37a6ac72ff2e7aabce7f1489f543391de89efce
Joey.Huang <kamidox@qq.com>
2018-07-09 02:59:07 +0800

    update data source to 163

--- data_source.py
+++ data_source.py
@@ -9,12 +9,16 @@ import numpy as np
 def retrive_stock_data(stockid, folder):
     """ 下载整个股票数据 """
 
-    url = 'http://table.finance.yahoo.com/table.csv?s=%s' % (stockid)
+    end = datetime.date.today().strftime('%Y%m%d')
+    url = 'http://quotes.money.163.com/service/chddata.html?' \
+          'code=%s&start=19900101&end=%s' \
+          '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;PCHG;TURNOVER;VOTURNOVER;VATURNOVER' \
+          % (stockid, end)
     print('downloading %s to %s from %s' % (stockid, folder, url))
-    fname = os.path.join(folder, '%s.csv' % stockid.split('.')[0])
+    fname = os.path.join(folder, '%s.csv' % stockid[-6:])
     if not os.path.isdir(folder):
         os.mkdir(folder)
-    urllib.urlretrieve(url, fname)
+    urllib.request.urlretrieve(url, fname)
 
 
 def update_stock_data(stockid, folder, startdate=None):
@@ -28,7 +32,7 @@ def update_stock_data(stockid, folder, startdate=None):
     if not os.path.isdir(folder):
         os.mkdir(folder)
 
-    fname = os.path.join(folder, '%s.csv' % stockid.split('.')[0])
+    fname = os.path.join(folder, '%s.csv' % stockid[-6:])
     if startdate is None and not os.path.exists(fname):
         retrive_stock_data(stockid, folder)
         return
@@ -36,33 +40,29 @@ def update_stock_data(stockid, folder, startdate=None):
     data = None
     last_date = None
     if os.path.exists(fname):
-        data = pd.read_csv(fname, index_col='Date', parse_dates=True)
+        data = pd.read_csv(fname, index_col='日期', parse_dates=True)
         last_date = data.iloc[0:1].index.tolist()[0]
 
     startdate = pd.Timestamp(startdate)
     if last_date and startdate < last_date:
-        startdate = last_date
+        startdate = last_date + pd.Timedelta(days=1)
     today = pd.Timestamp(datetime.date.today())
-    if today - startdate < pd.Timedelta(days=2):
+    if today - startdate < pd.Timedelta(days=1):
         print('Nothing to update. %s last date is %s.' % (stockid, last_date))
         return
 
-    query = [
-        ('a', startdate.month - 1),
-        ('b', startdate.day),
-        ('c', startdate.year),
-        ('d', today.month - 1),
-        ('e', today.day),
-        ('f', today.year),
-        ('s', stockid),
-    ]
-    url = 'http://table.finance.yahoo.com/table.csv?%s' % urllib.urlencode(query)
+    start = startdate.date().strftime('%Y%m%d')
+    end = today.date().strftime('%Y%m%d')
+    url = 'http://quotes.money.163.com/service/chddata.html?' \
+          'code=%s&start=%s&end=%s' \
+          '&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;PCHG;TURNOVER;VOTURNOVER;VATURNOVER' \
+          % (stockid, start, end)
     print('updatting %s [%s - %s] form %s' % (stockid, startdate.date(), today.date(), url))
     temp_file = fname + '.tmp'
     try:
-        urllib.urlretrieve(url, temp_file)
-        update_data = pd.read_csv(temp_file, index_col='Date', parse_dates=True)
-    except Exception, e:
+        urllib.request.urlretrieve(url, temp_file)
+        update_data = pd.read_csv(temp_file, index_col='日期', parse_dates=True, encoding='gb18030')
+    except Exception as e:
         print(e)
         return
 
@@ -75,20 +75,20 @@ def update_stock_data(stockid, folder, startdate=None):
     os.unlink(temp_file)
 
 
-def stock_list(files, postfixs):
-    """ 合并股票列表，输出合并后的，可以通过 yahoo api 获取的股票列表
+def stock_list(files, marks):
+    """ 合并股票列表，输出合并后的，可以通过 api 获取的股票列表
 
     files: a sequence like ['SH.txt', 'SZ.txt']
-    postfixs: a sequence map to files, like ['.ss', '.sz']
+    marks: a sequence map to files, like ['0', '1']
     """
-    if len(files) != len(postfixs):
-        print('error: size of files and postfixs not match.')
+    if len(files) != len(marks):
+        print('error: size of files and marks not match.')
         return
 
     stocks = []
     for i in range(len(files)):
-        data = pd.read_csv(files[i], header=None, names=['name', 'id'], dtype={'id': np.string0}, skipinitialspace=True)
-        data['id'] += postfixs[i]
+        data = pd.read_csv(files[i], header=None, names=['name', 'id'], dtype={'id': np.str}, skipinitialspace=True)
+        data['id'] = marks[i] + data['id']
         stocks.append(data)
 
     data = pd.concat(stocks)
@@ -99,13 +99,14 @@ def stock_list(files, postfixs):
 def update_stock_data_batch(filter=None, startdate=None):
     """ 批量更新所有股票数据 """
 
-    slist = stock_list(['SH.txt', 'SZ.txt'], ['.ss', '.sz'])
+    slist = stock_list(['SH.txt', 'SZ.txt'], ['0', '1'])
     if filter:
-        slist = slist[slist['id'].str.startswith(filter)]
+        slist = slist[slist['id'].str.endswith(filter)]
     for i in range(len(slist)):
         s = slist.iloc[i]
-        update_stock_data(s['id'], 'yahoo-data', startdate=startdate)
+        update_stock_data(s['id'], 'stock-data', startdate=startdate)
+
 
 if __name__ == '__main__':
-    update_stock_data_batch('002', startdate='2015-10-1')
+    update_stock_data_batch('600690', startdate='1990-01-01')
 
